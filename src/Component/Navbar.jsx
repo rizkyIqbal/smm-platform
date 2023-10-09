0fc321dfbaee4bf478cc7925e6a1fab47d6b0940
Tanfitra <tanfitraadhim@gmail.com>
2023-10-09 20:19:48 +0700

    fix:footer text

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,17 +1,37 @@
 import { useState } from 'react'
 import logo from '../assets/img/logo.png'
 import { Link } from "react-router-dom";
+import { Popover } from '@headlessui/react'
+import { HiMenuAlt3 } from "react-icons/hi";
 
 function Navbar() {
   return (
     <>
-      <div className='w-screen h-32 bg-[#D988B9] px-16'>
-        <div className='w-[475px] h-full'>
-            <div className='grid grid-cols-3 h-full'>
-
+      <div className='w-screen h-32 bg-[#D988B9] lg:px-16 px-6'>
+        <div className='lg:w-[475px] w-full h-full'>
+            <div className='lg:grid lg:grid-cols-3 h-full flex justify-between items-center'>
                 <div className='w-28 h-auto'>
-                    <img src={logo} alt="" />
+                    <img src={logo} alt="" /> 
                 </div>
+                {/* <div>
+                  test
+                </div> */}
+                <Popover className="lg:hidden relative">
+                  <Popover.Button>
+                    <HiMenuAlt3 fontSize="2.5rem"/>
+                  </Popover.Button>
+
+                  <Popover.Panel className="absolute z-10">
+                    <div className="grid grid-cols-2">
+                      <Link href="#" className="text-white font-medium hover:text-white">Home</Link>
+                      <Link href="#" className="text-white font-medium hover:text-white">Order</Link>
+                      <Link href="#" className="text-white font-medium hover:text-white">About Us</Link>
+                      <Link href="#" className="text-white font-medium hover:text-white">Contact</Link>
+                    </div>
+
+                    <img src="/solutions.jpg" alt="" />
+                  </Popover.Panel>
+                </Popover>
                 <div className="hidden md:flex space-x-4 col-span-2 py-12">
                     <Link href="#" className="text-white font-medium hover:text-white">Home</Link>
                     <Link href="#" className="text-white font-medium hover:text-white">Order</Link>
