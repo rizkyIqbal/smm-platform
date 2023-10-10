20173ff58bfc0898ea301a3e3d4cdf60d5a02c7e
rizkyIqbal <arizkyiqbal@gmail.com>
2023-10-10 20:05:33 +0700

    feat: add login and register route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Dashboard from './Pages/Dashboard'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
@@ -9,7 +11,8 @@ function App() {
   return (
     <Routes>
       <Route index element={<Dashboard />} />
-      {/* <Route path="/login" element={<Login />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
