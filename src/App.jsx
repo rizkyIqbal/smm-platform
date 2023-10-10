febed7c73131d164aead039c1c5aaf8a3a605b53
arizkyiqbal <arizkyiqbal@gmail.com>
2023-10-10 18:03:39 +0700

    feat: add order route to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from '/vite.svg'
 import Dashboard from './Pages/Dashboard'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Order from './Pages/Order'
 
 function App() {
   return (
     <Routes>
       <Route index element={<Dashboard />} />
+      <Route path="/order" element={<Order />} />
       {/* <Route path="/login" element={<Login />} /> */}
     </Routes>
   )
