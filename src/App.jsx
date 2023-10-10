cc5433314ec3c8563159a5d5562c498f91e5c560
arizkyiqbal <arizkyiqbal@gmail.com>
2023-10-10 17:29:58 +0700

    chore: add routes to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Dashboard from './Pages/Dashboard'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <Dashboard />
-    </>
+    <Routes>
+      <Route index element={<Dashboard />} />
+      {/* <Route path="/login" element={<Login />} /> */}
+    </Routes>
   )
 }
 
