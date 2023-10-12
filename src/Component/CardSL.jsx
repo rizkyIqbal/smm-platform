94d1900aba4a0f6c05fc362ad6dbd06cd850beeb
Tanfitra <tanfitraadhim@gmail.com>
2023-10-12 13:59:39 +0700

    fix: card & dropdown

--- src/Component/CardSL.jsx
+++ src/Component/CardSL.jsx
@@ -1,38 +1,40 @@
 import { useState } from 'react'
-import {BiLogoInstagramAlt} from 'react-icons/bi'
+import {BsInstagram} from 'react-icons/bs'
 import {BiLogoTiktok} from 'react-icons/bi'
-import {BsFillPersonFill} from 'react-icons/bs'
-import {BiTask} from 'react-icons/bi'
+import {GoBook} from 'react-icons/go'
+import {FiClipboard} from 'react-icons/fi'
 import { AiFillStar } from "react-icons/ai";
+import {IoLocationSharp} from "react-icons/io5"
 
 function CardSL() {
   return (
     <>
-    <div className="flex justify-center max-w-sm bg-white border border-gray-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
-        <div className="flex flex-col items-center pb-10 mt-8 -mb-8">
-            <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/src/assets/img/profile/profile1.png" alt="Juliana img"/>
-            <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Juliana Santika</h5>
-            <span className="text-sm text-gray-500 dark:text-gray-400">Depok, Jawa Barat</span>
-            <div className="flex mt-4 space-x-3 md:mt-6">
-              <BsFillPersonFill color='' fontSize="40px" />
-              <p className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-black">1 Week</p>
+    <div className="flex justify-center w-[288px] bg-white border border-gray-400 rounded-lg shadow">
+        <div className="flex flex-col items-center pb-10 mt-8 -mb-4">
+            <img className="w-36 h-36 mb-3 rounded-full shadow-lg" src="/src/assets/img/profile/profile1.png" alt="Juliana img"/>
+            <h5 className=" text-[16px] font-medium text-gray-900">Juliana Santika</h5>
+            <div className="flex">
+              <IoLocationSharp color='grey' fontSize="16px"/>
+              <span className="text-[10px] text-gray-500">Depok, Jawa Barat</span>
             </div>
-            <div className="flex mt-4 space-x-3 md:mt-6">
-            <BiTask color='' fontSize="40px" />
-              <p className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-black">5 People</p>
-            </div>
-            <div className="flex space-x-3 md:mt-6 pl-3 pr-3 text-sm font-medium text-center text-black">
+            <div className="flex space-x-3 md:mt-2 pl-3 pr-3 text-sm font-bold text-center text-primary">
               <p>Rp. 350.000 -  700.000</p>
             </div>
-            <div className="flex space-x-3 md:mt-6">
-              <AiFillStar color='gold' fontSize="40px"/>
-              <p className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-black">3.9 | 15 Deals</p>
+            <div className="flex mt-4 space-x-2 md:mt-2 justify-center">
+              <FiClipboard color='' fontSize="16px" />
+              <p className="inline-flex items-center  text-xs font-medium text-center text-black">1 Week</p>
+              <GoBook color='' fontSize="16px" />
+              <p className="inline-flex items-center text-xs font-medium text-center text-black">5 People</p>
             </div>
-            <div className="flex mt-4 space-x-3 md:mt-6 pl-3 pr-3">
-                <BiLogoInstagramAlt color='' fontSize="45px" />
-                <a href="https://www.instagram.com/" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-black hover:text-black">10k</a>
-                <BiLogoTiktok color='' fontSize="45px" />
-                <a href="https://www.tiktok.com/" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-black hover:text-black">10k</a>
+            <div className="flex space-x-2 md:mt-2 justify-center">
+              <AiFillStar color='gold' fontSize="24px"/>
+              <p className="inline-flex items-center text-xs font-medium text-center text-black">3.9 | 15 orders</p>
+            </div>
+            <div className="flex space-x-2 md:mt-2 justify-center ">
+                <BsInstagram color='' fontSize="24px" />
+                <a href="https://www.instagram.com/" className="inline-flex items-center text-xs font-medium text-center text-black hover:text-black">10k</a>
+                <BiLogoTiktok color='' fontSize="24px" />
+                <a href="https://www.tiktok.com/" className="inline-flex items-center text-sm font-medium text-center text-black hover:text-black">10k</a>
             </div>
         </div>
     </div>
--- src/Pages/SellerList.jsx
+++ src/Pages/SellerList.jsx
@@ -12,27 +12,110 @@ function SellerList() {
             <Navbar/>
             <div className='mt-8 mb-8 flex justify-center w-full'>               
                 <form>   
-                    <label for="search" class="mb-2 text-lg font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                    <label for="search" class="mb-2 text-lg font-medium text-gray-900 sr-only">Search</label>
                     <div class="relative w-full">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <svg class="w-4 h-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                             </svg>
                         </div>
-                        <input type="search" id="search" class="block w-full p-4 pl-10 text-lg text-gray-900 border border-gray-700 rounded-xl bg-gray-50 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Search" required/>
-                        <button type="submit" class="text-white absolute right-2 bottom-2 bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-lg px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Search</button>
+                        <input type="search" id="search" class="block w-full p-4 pl-10 text-lg text-gray-900 border border-gray-700 rounded-xl bg-gray-50 focus:ring-gray-500 focus:border-gray-500" placeholder="Search" required/>
+                        <button type="submit" class="text-white absolute right-2 bottom-2 bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-lg px-4 py-2">Search</button>
                     </div>
                 </form>
             </div>
 
-            {/* <div>
-                <button id="dropdownBgHoverButton" data-dropdown-toggle="dropdownBgHover" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown checkbox <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                </svg>
-                </button>
-            </div> */}
+            
+            <div className='pl-32 relative'>
+                <div className='flex p-4 absolute top-8 justify-center w-80 border border-gray-500 rounded-t-3xl'>
+                    <button className="justify-center text-white bg-secondary font-medium w-52 rounded-xl text-lg p-1 text-center inline-flex items-center flex" type="button">Kategori<svg class="w-2.5 h-2.5 ml-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                    </svg>
+                    </button>
+                </div>
 
-            <div className="grid grid-cols-2 gap-12 absolute right-[85px] border-gray-600">
+                <div className='p-4 absolute top-[102px] w-80 border border-gray-500 rounded-none'>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Food</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Fashion</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Kitchenware</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Sport</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Others</label>
+                    </div>
+                    <div className='flex p-3 -ml-3 items-center justify-center w-80'>
+                        <button className="justify-center text-white bg-secondary font-medium w-52  rounded-xl text-lg p-1 text-center inline-flex items-center flex" type="button">Influencer<svg class="w-2.5 h-2.5 ml-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                        </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <div className='p-4 absolute top-[436px] w-80 border border-gray-500 rounded-none'>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">IG Nano (5k-15k)</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">IG Micro (50k-200k)</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">IG Macro (300k-500k)</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">IG Mega (500k++)</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Tiktok Nano (5k-15k)</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Tiktok Micro (50k-200k)</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Tiktok Macro (300k-500k)</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Tiktok Mega (500k++)</label>
+                    </div>
+                    <div className='flex p-3 -ml-3 items-center justify-center w-80'>
+                        <button className="justify-center text-white bg-secondary font-medium w-52  rounded-xl text-lg p-1 text-center inline-flex items-center flex" type="button">Gender<svg class="w-2.5 h-2.5 ml-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                        </svg>
+                        </button>
+                    </div>
+                </div>
+                <div className='p-4 absolute top-[915px] w-80 border border-gray-500 rounded-b-xl'>
+                <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Laki-laki</label>
+                    </div>
+                    <div class="flex items-center mb-4">
+                        <input id="default-checkbox" type="checkbox" value="" class="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                        <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900">Perempuan</label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-2 gap-16 absolute right-[155px] border-gray-600 mt-8">
                 <CardSL />
                 <CardSL />
                 <CardSL />
@@ -41,7 +124,7 @@ function SellerList() {
                 <CardSL />
             </div>
             
-            <div className='w-screen absolute bottom-[-1650px]'>
+            <div className='w-screen absolute bottom-[-1150px]'>
             <Footer/>
             </div>
             
