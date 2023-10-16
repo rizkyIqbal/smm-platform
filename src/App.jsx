0e4c2f8567adb4fd67e4e9a7fdf4dded2515cac9
Iqbal Rizky <arizkyiqbal@gmail.com>
2023-10-16 23:53:22 +0700

    feat: add SMMOrder route

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import Faq from './Pages/Faq'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Order from './Pages/Order'
+import SMMOrder from './Pages/SMMOrder'
 
 function App() {
   return (
     <Routes>
       <Route index element={<Dashboard />} />
-      <Route path="/order" element={<Order />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/order" element={<Order />} />
       <Route path="/seller" element={<SellerList/>} />
       <Route path="/faq" element={<Faq/>} />
+      <Route path="/order/smm" element={<SMMOrder />} />
     </Routes>
   )
 }
