d5d601c0e793ef3c0ba1ea34822dbf34bfe63caa
Tanfitra <tanfitraadhim@gmail.com>
2023-10-17 21:23:59 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import Faq from './Pages/Faq'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Order from './Pages/Order'
-
+import SellerDetail from './Pages/SellerDetail'
+import SMMOrder from './Pages/SMMOrder'
+import AboutUs from './Pages/AboutUs'
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
       <Route path="/order" element={<Order />} />
       <Route path="/seller" element={<SellerList/>} />
       <Route path="/faq" element={<Faq/>} />
+      <Route path="/sellerdetail" element={<SellerDetail/>} />
+      <Route path="/order/smm" element={<SMMOrder />} />
+      <Route path="/about" element={<AboutUs />} />
     </Routes>
   )
 }
